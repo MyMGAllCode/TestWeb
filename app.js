196961014efff699f2863a70354c662c123859e0
Dharmendra  Kumar Prajapati <dktech89@gmail.com>
2020-02-19 23:47:29 +0530

    Node api commit

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 console.log('Welcome!!');
+var express = require('express');
+var app = express();
+//var fs = require("fs");
+
+app.get('/test', function (req, res) {
+   
+      res.end( "Welcome!! Node Api in EC2");
+ 
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+})
